608b404a4babafa50a783c57930b6fa59da0d5fa
Alpay Başol <alpay41@gmail.com>
2023-06-02 16:20:01 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class DusenNesneKuvveti
+{
+    public static void KuvvetiHesapla(double yukseklik, double agirlik)
+    {
+        const double yerCekimi = 9.8; // Yer çekimi ivmesi (m/s^2)
+        double hiz = Math.Sqrt(2 * yerCekimi * yukseklik); // Düşme hızını hesapla
+        double kuvvet = agirlik * hiz; // Kuvveti hesapla
+
+        Console.WriteLine("Nesnenin Yere Uyguladığı Kuvvet: " + kuvvet.ToString("N2") + " N");
+    }
+
+    public static void Main(string[] args)
+    {
+        // Örnek kullanım
+        double dusmeYuksekligi = 50.0; // Düşme yüksekliği (metre)
+        double agirlik = 70.0; // Nesnenin ağırlığı (kilogram)
+
+        KuvvetiHesapla(dusmeYuksekligi, agirlik);
+    }
+}
